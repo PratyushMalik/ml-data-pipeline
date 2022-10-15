e166def25d3a74016270c0a1f83b87b007df5695
PratyushMalik <pratyush0410@gmail.com>
2022-10-15 12:53:17 +0530

    requirements updated

--- src/kafka_config/__init__.py
+++ src/kafka_config/__init__.py
@@ -1,18 +1,20 @@
 
 import os
 
-
-SECURITY_PROTOCOL="SASL_SSL"
-SSL_MACHENISM="PLAIN"
+# Cloud API details
 API_KEY = os.getenv('API_KEY',None)
-ENDPOINT_SCHEMA_URL  = os.getenv('ENDPOINT_SCHEMA_URL',None)
 API_SECRET_KEY = os.getenv('API_SECRET_KEY',None)
 BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER',None)
-SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
-SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
+
+# Schema registry details
+ENDPOINT_SCHEMA_URL  = os.getenv('ENDPOINT_SCHEMA_URL',None)
 SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY',None)
 SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
-
+# Security details
+# SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
+# SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
+SECURITY_PROTOCOL="SASL_SSL"
+SSL_MACHENISM="PLAIN"
 
 def sasl_conf():
 
@@ -24,7 +26,7 @@ def sasl_conf():
                 'sasl.username': API_KEY,
                 'sasl.password': API_SECRET_KEY
                 }
-    print(sasl_conf)
+    # print(sasl_conf)
     return sasl_conf
 
 
